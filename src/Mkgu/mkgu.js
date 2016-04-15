42ebac94aac1999cb4befa06a059d558bd14abee
YuuKohaku <soul.of.bytes@gmail.com>
2016-04-15 17:39:21 +0600

    1

--- src/Mkgu/mkgu.js
+++ src/Mkgu/mkgu.js
@@ -1,11 +1,14 @@
 'use strict'
 
+let curl = require('node-libcurl')
+	.Curl;
+
 const ns_main = 'http://vashkontrol.ru#';
 const ns_w3 = 'http://www.w3.org/2000/xmlns/';
 const ns_dsig = 'http://www.w3.org/2000/09/xmldsig#';
 const ns_ec = 'http://www.w3.org/2001/10/xml-exc-c14n#';
-const ns_events = 'https://vashkontrol.ru/hershel/events';
-const ns_rates = 'https://vashkontrol.ru/hershel/rates';
+const ns_events = 'https://vashkontrol.ru/hershel/sandbox/events.json';
+const ns_rates = 'https://vashkontrol.ru/hershel/sandbox/rates.json';
 
 class Mkgu {
 	constructor() {
@@ -15,7 +18,13 @@ class Mkgu {
 	init(cfg) {
 		this.certificate_pub = cfg.certificate_pub || './keys/cert.pem';
 		this.certificate_priv = cfg.certificate_priv || './keys/key.pem';
-		this.vendor_id = = cfg.vendor_id;
+
+		this.vendor_id = cfg.vendor_id;
+	}
+
+	launch() {
+		console.log("HELL");
+		return Promise.resolve(true);
 	}
 
 	//API
